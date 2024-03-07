1259fe36348abf37b141aa225fa8f9cc322ab466
Landibar19 <99284691+Landibar19@users.noreply.github.com>
2024-03-07 18:07:36 +0100

    Update NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -156,7 +156,7 @@ export default function NavBar() {
     sx={{ 
       display: 'flex', 
       flexDirection: 'row', 
-      height: '60px',
+      height: '40vw',
     
       }}>
         <div>
